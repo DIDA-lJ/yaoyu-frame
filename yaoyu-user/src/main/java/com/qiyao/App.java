318983d37e6845bf2bb59f2573e72988fce2e8d8
linqi <wkw4399xs@gmail.com>
2023-08-26 02:06:32 +0800

    first commit 项目初始化架构的搭建，maven 构建，以及用于模块构建

---
+++ yaoyu-user/src/main/java/com/qiyao/App.java
@@ -0,0 +1,13 @@
+package com.qiyao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
